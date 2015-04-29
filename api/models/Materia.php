58ee9f802d55c162c30fe4a70b7571becc9c0fd8
Vytor Calixto <vytorcalixto@gmail.com>
2015-04-29 05:33:43 +0000

    Model Professor

--- api/models/Materia.php
+++ api/models/Materia.php
@@ -3,7 +3,6 @@
 use Phalcon\Mvc\Model;
 use Phalcon\Mvc\Model\Message;
 use Phalcon\Mvc\Model\Validator\Uniqueness;
-use Phalcon\Mvc\Model\Validator\InclusionIn;
 
 class Materia extends Model{
     private $codigo;
